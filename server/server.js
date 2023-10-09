49c5f505455bbb896c93ce54c0d6cd65bffb2c04
ganeshkrishna1 <ganeshkrishnapagadala@gmail.com>
2023-10-09 08:09:07 +0530

    SQL server connected

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express"
+import mysql from "mysql"
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database:"project4",
+})
+
+con.connect(function(err) {
+    if(err) { 
+        console.log("Error in Connection");
+        console.log(err);
+    } else {
+        console.log("SQL server Connected");
+    }
+})
+app.listen(8081, ()=> {
+    console.log("Running");
+})
