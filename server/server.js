74329fe881b82cb3fcd5d8920659fa0c0bd8e06d
yaswanthsk <yaswanthsaikumar26@gmail.com>
2023-10-11 14:08:46 +0530

    LandLord Homepage with Add property details

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import mysql from "mysql"
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import bodyParser from "body-parser";
+import multer from "multer";
+import path from 'path'; 
 
 const app = express();
 app.use(cors());
@@ -28,6 +30,19 @@ con.connect(function(err) {
 app.listen(8081, ()=> {
     console.log("Running");
 })
+
+// Set storage engine
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Store property images in a specific folder
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
+});
+
+// Initialize Multer
+const upload = multer({ storage: storage });
 
 
 app.post('/signup', (req, res) => {
@@ -75,13 +90,75 @@ app.post('/signup', (req, res) => {
         console.error('Database error:', err);
         return res.status(500).json({ message: 'Server error' });
       }
-  
       if (results.length === 1) {
         // Successful login
-        return res.status(200).json({ message: 'Login successful' });
+        if (userType === 'landlord') {
+          const landlordId = results[0].landlord_id; // Assuming there's a "landlord_id" field in the database
+          console.log('Logged in as a landlord. Landlord ID:', landlordId);
+          return res.status(200).json({ message: 'Login successful', landlordId });
+        } else if (userType === 'user') {
+          const userId = results[0].user_id; // Assuming there's a "user_id" field in the database
+          console.log('Logged in as a user. User ID:', userId);
+          return res.status(200).json({ message: 'Login successful', userId });
+        } else {
+          return res.status(200).json({ message: 'Login successful' });
+        }
       } else {
         // Invalid credentials
         return res.status(401).json({ message: 'Invalid credentials' });
       }
     });
   });
+
+
+/* // Create an API endpoint to handle property image uploads
+app.post('/properties/add', upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded.' });
+  }
+  const imagePath = req.file.path;
+  const { landlord_id, property_type, location, rent, bedrooms, max_members, description } = req.body;
+
+  // Save the imagePath in the 'properties' table
+  const sql = 'INSERT INTO properties (landlord_id, property_type, location, rent, bedrooms, max_members, description, image_path) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+  const values = [landlord_id, property_type, location, rent, bedrooms, max_members, description, imagePath];
+
+  con.query(sql, values, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to upload property.' });
+    }
+    res.json({ success: 'Property uploaded successfully.' });
+  });
+}); */
+
+// Create an API endpoint to handle property image uploads
+app.post('/properties/add', upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded.' });
+  }
+  
+  // Log data sent to the server
+  console.log('Received data from the frontend:');
+  console.log('landlord_id:', req.body.landlord_id);
+  console.log('property_type:', req.body.property_type);
+  console.log('location:', req.body.location);
+  console.log('rent:', req.body.rent);
+  console.log('bedrooms:', req.body.bedrooms);
+  console.log('max_members:', req.body.max_members);
+  console.log('description:', req.body.description);
+  console.log('imagePath:', req.file.path);
+
+  const imagePath = req.file.path;
+  const { landlord_id, property_type, location, rent, bedrooms, max_members, description } = req.body;
+
+  // Save the imagePath in the 'properties' table
+  const sql = 'INSERT INTO properties (landlord_id, property_type, location, rent, bedrooms, max_members, description, image_path) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+  const values = [landlord_id, property_type, location, rent, bedrooms, max_members, description, imagePath];
+
+  con.query(sql, values, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to upload property.' });
+    }
+    res.json({ success: 'Property uploaded successfully.' });
+  });
+});
--- src/components/Landlord/Landlord.js
+++ src/components/Landlord/Landlord.js
@@ -1,7 +1,151 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function Landlord() {
-    return(
-        <h1>Landlord Logged in Successful</h1>
-    )
+  const [formData, setFormData] = useState({
+    landlord_id: '', // Initialize landlord_id as an empty string
+    property_type: '',
+    location: '',
+    rent: '',
+    bedrooms: '',
+    max_members: '',
+    description: '',
+  });
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    // Retrieve landlordId from localStorage when the component mounts
+    const landlordId = localStorage.getItem('landlordId');
+    if (landlordId) {
+      setFormData((prevData) => ({
+        ...prevData,
+        landlord_id: landlordId, // Set landlordId in the component state
+      }));
+    }
+  }, []); // Empty dependency array to run the effect only once
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleImageUpload = (event) => {
+    const selectedImage = event.target.files[0];
+    setImage(selectedImage);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formDataToSend = new FormData();
+    formDataToSend.append('landlord_id', formData.landlord_id);
+    formDataToSend.append('property_type', formData.property_type);
+    formDataToSend.append('location', formData.location);
+    formDataToSend.append('rent', formData.rent);
+    formDataToSend.append('bedrooms', formData.bedrooms);
+    formDataToSend.append('max_members', formData.max_members);
+    formDataToSend.append('description', formData.description);
+    formDataToSend.append('image', image);
+
+    try {
+      const response = await axios.post('http://localhost:8081/properties/add', formDataToSend, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      if (response.status === 200) {
+        alert('Property added successfully.');
+      }
+    } catch (err) {
+      console.error(err);
+      alert('An error occurred while adding the property.');
+    }
+  };
+
+  return (
+    <div className="landlord-container">
+      <h2>Add Property</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="property_type">Property Type</label>
+          <input
+            type="text"
+            id="property_type"
+            name="property_type"
+            value={formData.property_type}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="location">Location</label>
+          <input
+            type="text"
+            id="location"
+            name="location"
+            value={formData.location}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="rent">Rent</label>
+          <input
+            type="number"
+            id="rent"
+            name="rent"
+            value={formData.rent}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="bedrooms">Bedrooms</label>
+          <input
+            type="number"
+            id="bedrooms"
+            name="bedrooms"
+            value={formData.bedrooms}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="max_members">Max Members</label>
+          <input
+            type="number"
+            id="max_members"
+            name="max_members"
+            value={formData.max_members}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="image">Image Upload</label>
+          <input
+            type="file"
+            id="image"
+            name="image"  // Change name to "image"
+            accept="image/*"
+            onChange={handleImageUpload}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
+
 export default Landlord;
